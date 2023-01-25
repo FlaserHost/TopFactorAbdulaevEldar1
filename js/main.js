233d8536cff2bf727287b8bd7bc4cb00f86b8a05
Abdullaev Eldar <107851424+Abdullaev-Eldar@users.noreply.github.com>
2023-01-25 21:40:58 +0700

    blok 1 media

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+// _______Бургер меню_______
+const menuIcon = document.querySelector('#menu-icon');
+const popup = document.querySelector('#popup');
+const body = document.body;
+
+// Клонируем меню, чтобы задать свои стили для мобильной версии
+const menu = document.querySelector('#menu-list').cloneNode(1);
+
+// При клике на иконку hamb вызываем ф-ию hambHandler
+menuIcon.addEventListener('click', menuIconHandler);
+
+// Выполняем действия при клике ..
+function menuIconHandler(e) {
+  e.preventDefault();
+  // Переключаем стили элементов при клике
+  popup.classList.toggle('open');
+  menuIcon.classList.toggle('active');
+  body.classList.toggle('noscroll');
+  renderPopup();
+}
+
+// Здесь мы рендерим элементы в наш попап
+function renderPopup() {
+  popup.appendChild(menu);
+}
+
+// Код для закрытия меню при нажатии на ссылку
+const links = Array.from(menu.children);
+
+// Для каждого элемента меню при клике вызываем ф-ию
+links.forEach((link) => {
+  link.addEventListener('click', closeOnClick);
+});
+
+// Закрытие попапа при клике на меню
+function closeOnClick() {
+  popup.classList.remove('open');
+  menuIcon.classList.remove('active');
+  body.classList.remove('noscroll');
+}
+
+// _______Слайдер_______
 const dots = document.getElementsByClassName('dots-item');
 const dotsArea = document.getElementsByClassName('dots-block')[0];
 const slides = document.getElementsByClassName('slider-item');
@@ -26,9 +68,21 @@ function plusSlide(n) {
   showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
 prevBtn.onclick = function () {
   plusSlide(-1);
 };
 nextBtn.onclick = function () {
   plusSlide(1);
+};
+
+dotsArea.onclick = function (e) {
+  for (let i = 0; i < dots.length + 1; i++) {
+    if (e.target.classList.contains('dots-item') && e.target === dots[i - 1]) {
+      currentSlide(i);
+    }
+  }
 };
